960d1d8ffd741aa425a5297086fec87ebf6af0f6
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2012-05-17 23:36:42 +0400

    Alpha

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'rest-client'
+require 'json'
+
+DB = ENV['CLOUDANT_URL' + '/history' || 'localhost:5984' + '/history'¬
 
 get '/' do
-  "Hello, world"
+  doc = RestClient.get("#{DB}/_all_docs")
+  @messages = JSON.parse(doc)
+  erb :index
+end
+
+get '/:id' do
+  message = RestClient.get("#{DB}/#{params[:id]}")
+  @message = JSON.parse(message)
+  erb :message
 end
