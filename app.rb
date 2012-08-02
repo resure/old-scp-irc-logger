e4c51d64040723af5d9106d8201a8ffc91bae9d7
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2012-08-02 06:16:47 +0400

    Update views

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require 'sinatra'
 require 'rest-client'
 require 'json'
 
-if ENV['CLOUDANT_URL']
-  DB = ENV['CLOUDANT_URL'] + '/history'
-else
-  DB = 'localhost:5984' + '/history'
-end
+# if ENV['CLOUDANT_URL']
+#   DB = ENV['CLOUDANT_URL'] + '/history'
+# else
+#   DB = 'localhost:5984' + '/history'
+# end
+
+DB = 'https://app4695911.heroku:HecgqPnwGJjNgIHsuJYGshbj@app4695911.heroku.cloudant.com/history'
 
 get '/' do
   doc = RestClient.get("#{DB}/_all_docs?include_docs=true")
