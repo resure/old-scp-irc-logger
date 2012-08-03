d6a79b9bbdbf5fada26a83a62c79188cee0431f1
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2012-08-03 16:39:44 +0400

    Limit & offset

--- app.rb
+++ app.rb
@@ -2,14 +2,18 @@ require 'sinatra'
 require 'rest-client'
 require 'json'
 
-if ENV['CLOUDANT_URL']
-  DB = ENV['CLOUDANT_URL'] + '/history'
-else
-  DB = 'localhost:5984' + '/history'
-end
+# if ENV['CLOUDANT_URL']
+#   DB = ENV['CLOUDANT_URL'] + '/history'
+# else
+#   DB = 'localhost:5984' + '/history'
+# end
+
+DB = 'https://app4695911.heroku:HecgqPnwGJjNgIHsuJYGshbj@app4695911.heroku.cloudant.com/history'
 
 get '/' do
-  doc = RestClient.get("#{DB}/_all_docs?include_docs=true")
+  params[:limit] ||= 100
+  params[:skip] ||= 0
+  doc = RestClient.get("#{DB}/_all_docs?include_docs=true&descending=true&limit=#{params[:limit].to_i}&skip=#{params[:skip].to_i}")
   @messages = JSON.parse(doc)
   erb :index
 end
